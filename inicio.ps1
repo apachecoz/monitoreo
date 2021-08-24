2aa7963850c79ff644a8b5bf02eae359efb17b50
Adrian Pacheco <apachecoz@gmail.com>
2021-08-24 10:59:00 -0500

    Version 1.0

---
+++ inicio.ps1
@@ -0,0 +1,11 @@
+# Software de Monitoreo Multipropósito CDA
+<#
+    Autores:    Adrián Pacheco
+                Julio Montalvo
+.Synopsis
+    Realiza un bitacoreo de servicios determinados por el administrador
+
+#>
+
+
+
